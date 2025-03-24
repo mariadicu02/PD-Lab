06c32304ceb2bb2cb69ddd8127a3a5a20b2af0de
Maria Dicu <107000956+mariadicu02@users.noreply.github.com>
2025-03-24 08:56:11 +0200

    Add files via upload
    Driver manipulation main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,65 +1,69 @@
-#include <windows.h>
-#include <stdio.h>
-#include <tchar.h>
+﻿#include <windows.h>
+#include <setupapi.h>
+#include <initguid.h>
+#include <usbiodef.h>
+#include <iostream>
+#include <devguid.h>
+#include <cfgmgr32.h>
 
-void ReadImagePath(HKEY hKeyRoot, LPCTSTR pszPath) {
+#pragma comment(lib, "SetupAPI.lib")
 
-	HKEY hKey;
-	DWORD dwIndex = 0;
-	TCHAR szKeyName[256];
-	DWORD dwKeySize = sizeof(szKeyName)/ sizeof(TCHAR);
+void GetDeviceRegistryProperty(HDEVINFO hDevInfo, SP_DEVINFO_DATA devInfoData) {
+    DWORD DataT;
+    TCHAR buffer[1024];
+    DWORD buffersize = sizeof(buffer);
 
-	LONG lResult = RegOpenKeyEx(
-								hKeyRoot,
-								pszPath,
-								0,
-								KEY_READ,
-								&hKey 
-	);
-	if (lResult != ERROR_SUCCESS) {
-		_tprintf(_T("Eroare la deschiderea registrului, %ld\n"), lResult);
-		return;
-	}
-	while (RegEnumKeyEx(
-		hKey,
-		dwIndex,
-		szKeyName,
-		&dwKeySize,
-		NULL,
-		NULL,
-		NULL,
-		NULL) == ERROR_SUCCESS)
-	{
-		TCHAR szFullPath[512];
-		_stprintf_s(szFullPath, _T("%s\\%s"), pszPath, szKeyName);
-		HKEY hSubKey;
-		if (RegOpenKeyEx(hKeyRoot, szFullPath, 0, KEY_READ, &hSubKey) == ERROR_SUCCESS)
-		{
-			DWORD dwType;
-			TCHAR szImagePath[512];
-			DWORD dwSize = sizeof(szImagePath);
-			if (RegQueryValueEx(
-								hSubKey,
-								_T("Imagepath"),
-								NULL,
-								&dwType,
-								(LPBYTE)szImagePath,
-								&dwSize) == ERROR_SUCCESS)
-			{
-				if (dwType == REG_SZ || dwType == REG_EXPAND_SZ)
-				{
-					_tprintf(_T("Serviciu: %s\nPath: %s\n\n"), szKeyName, szImagePath);
-				}
-			}
-			RegCloseKey(hSubKey);
-		}
-		dwKeySize = sizeof(szKeyName) / sizeof(TCHAR);
-		dwIndex++;
-	}
-	RegCloseKey(hKey);
+    if (SetupDiGetDeviceRegistryProperty(hDevInfo, &devInfoData, SPDRP_FRIENDLYNAME, &DataT, (PBYTE)buffer, buffersize, NULL)) {
+        std::wcout << L"  Device Name: " << buffer << std::endl;
+        return;
+    }
+
+    if (SetupDiGetDeviceRegistryProperty(hDevInfo, &devInfoData, SPDRP_DEVICEDESC, &DataT, (PBYTE)buffer, buffersize, NULL)) {
+        std::wcout << L"  Device Description: " << buffer << std::endl;
+        return;
+    }
+
+    std::cerr << "  Could not retrieve any information about the device.\n";
 }
 
-int _tmain(void) {
-	ReadImagePath(HKEY_LOCAL_MACHINE, _T("SYSTEM\\CurrentControlSet\\Services"));
-	return 0;
+void ListUSBDevices() {
+    HDEVINFO hDevInfo;
+    SP_DEVICE_INTERFACE_DATA DeviceInterfaceData;
+    SP_DEVINFO_DATA devInfoData;
+    DWORD i;
+
+    hDevInfo = SetupDiGetClassDevs(&GUID_DEVINTERFACE_USB_DEVICE, 0, 0, DIGCF_PRESENT | DIGCF_DEVICEINTERFACE);
+    if (hDevInfo == INVALID_HANDLE_VALUE) {
+        std::cerr << "Error retrieving USB devices.\n";
+        return;
+    }
+
+    DeviceInterfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+    devInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+    for (i = 0; SetupDiEnumDeviceInterfaces(hDevInfo, NULL, &GUID_DEVINTERFACE_USB_DEVICE, i, &DeviceInterfaceData); i++) {
+        DWORD requiredSize = 0;
+        SetupDiGetDeviceInterfaceDetail(hDevInfo, &DeviceInterfaceData, NULL, 0, &requiredSize, NULL);
+        PSP_DEVICE_INTERFACE_DETAIL_DATA DeviceInterfaceDetailData = (PSP_DEVICE_INTERFACE_DETAIL_DATA)malloc(requiredSize);
+        if (!DeviceInterfaceDetailData) {
+            std::cerr << "Memory allocation error.\n";
+            continue;
+        }
+
+        DeviceInterfaceDetailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+
+        if (SetupDiGetDeviceInterfaceDetail(hDevInfo, &DeviceInterfaceData, DeviceInterfaceDetailData, requiredSize, NULL, &devInfoData)) {
+            std::wcout << L"USB Device: " << DeviceInterfaceDetailData->DevicePath << std::endl;
+            GetDeviceRegistryProperty(hDevInfo, devInfoData);
+        }
+
+        free(DeviceInterfaceDetailData);
+    }
+
+    SetupDiDestroyDeviceInfoList(hDevInfo);
+}
+
+int main() {
+    ListUSBDevices();
+    return 0;
 }
